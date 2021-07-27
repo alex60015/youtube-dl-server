2593c269d2e23f527d6d4c51b6ae2324db5c695d
alex60015 <13679384+alex60015@users.noreply.github.com>
2021-07-28 00:08:32 +0200

    Add docker registry to push image to

--- export_docker_image.sh
+++ export_docker_image.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -eu
 
-docker build . -t youtube-dl:local
-DOCKER_ID=$(docker ps -aqf "name=youtube-dl")
-docker export $DOCKER_ID -o youtube-dl.tar
+docker build . -t youtube-dl:latest
+DOCKER_ID=$(docker ps -aqf "name=youtube-dl:latest")
+docker tag youtube-dl:latest alex60015/youtube-dl:latest
+docker push alex60015/youtube-dl:latest
